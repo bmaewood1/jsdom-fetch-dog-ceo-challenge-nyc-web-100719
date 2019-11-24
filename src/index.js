1ee41f46f803a6c467e7d771f800dfc1ba8f6e2c
Bailey Wood <woodbmae@gmail.com>
2019-11-24 18:12:07 -0500

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('%c HI', 'color: firebrick')
+
+let message = []
+
+function getDogs(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(function(data){return data.json()})
+        .then(function(json){
+            array = Array.from(json.message)
+            ShowBreed(array)
+
+        })
+}
+
+getDogs()
+
+
+function ShowBreed(message){
+    let div = document.getElementById("dog-image-container")
+    message.forEach(function(message){
+        let img = document.createElement("img")
+        img.src = message
+        img.className = "breed-img"
+        div.append(img)
+    })
+}
